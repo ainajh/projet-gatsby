6c079ebd1613fed6ab4665b620ac0f0192ad8713
Aina Jh <ainajh11@gmail.com>
2022-02-04 00:50:50 +0300

    first commit

---
+++ src/components/main/Main.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Header from "../header/Header";
+import * as styles from "./main.module.scss";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Main;
